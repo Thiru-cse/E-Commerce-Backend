6dcb3912a82b7a18f4c4ef0384a1af6757318c89
Thiru-cse <thiru.c.cse@gmail.com>
2022-02-01 09:47:11 -0500

    Updated🎉

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ mongoose
   app.use("/api/orders", orderRoute);
   app.use("/api/checkout", stripeRoute);
 
-  app.listen(5000, () => {
+  app.listen(process.env.PORT || 5000, () => {
       console.log("Success");
   });
