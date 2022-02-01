8e76895f816ee45765d366bc22fa62852d905ea8
Thiru-cse <thiru.c.cse@gmail.com>
2022-02-01 11:20:37 -0500

    Updated🎉

--- index.js
+++ index.js
@@ -21,6 +21,17 @@ mongoose
 
   app.use(cors());
   app.use(express.json());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  
+    next();
+  });
+  
   app.use("/api/auth", authRoute);
   app.use("/api/users", userRoute);
   app.use("/api/products", productRoute);
