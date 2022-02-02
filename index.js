8230a14a848f862d96510f553f3f8f442890e873
Thiru-cse <thiru.c.cse@gmail.com>
2022-02-02 11:37:27 -0500

    Updated🎉

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ mongoose
     console.log(err);
   });
 
-//   app.all('*', function(req, res, next) {
-//    res.header('Access-Control-Allow-Origin', '*');
-//    res.header('Access-Control-Allow-Credentials', true);
-//    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//    res.header('Access-Control-Allow-Headers', 'Content-Type');
-//    next();
-//  });
-//   app.use(cors());
-//   app.use(express.json());
+  app.all('*', function(req, res, next) {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Credentials', true);
+   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+   res.header('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+ });
+  app.use(cors());
+  app.use(express.json());
 
-  app.use(
-    cors({
-      origin: "*",
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: "*",
+  //   })
+  // );
 
 app.use(express.json());
   app.use("/api/auth", authRoute);
